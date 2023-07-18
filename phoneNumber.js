1fc2da2498affe919050c66562cf7135c800d75a
Nick Kingston <ngrifk@gmail.com>
2023-07-18 09:32:33 -0500

    second commit

--- phoneNumber.js
+++ phoneNumber.js
@@ -9,16 +9,12 @@ Don’t forget the space after the closing parentheses!
 */
 
 function createPhoneNumber(numbers) {
-    if (numbers.length !== 10) {
-      throw new Error("Array must contain exactly 10 integers.");
-    }
-  
     const areaCode = numbers.slice(0, 3).join('');
     const firstPart = numbers.slice(3, 6).join('');
-    const secondPart = numbers.slice(6).join('');
-  
+    const secondPart = numbers.slice(6, 10).join('');
     return `(${areaCode}) ${firstPart}-${secondPart}`;
   }
+  
   
   // Usage:
   const phoneNumber = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
